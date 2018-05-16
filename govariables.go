cfe7d7ea515716260a5295b71fc84e9839e70c83
arjunthekkan <37802869+arjunthekkan@users.noreply.github.com>
2018-05-16 15:56:06 +0530

    Add files via upload

---
+++ govariables.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main()  {
+  var a ="golang"
+  fmt.Println(a)
+
+  var b,c int = 4,9
+  fmt.Println(b,c)
+
+  var d = false
+  fmt.Println(d)
+
+   e:= "GO LANGUAGE"
+  fmt.Println(e)
+
+
+}
