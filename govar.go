169d616fa72516ba47851d523620478d5dd42b73
arjunthekkan <37802869+arjunthekkan@users.noreply.github.com>
2018-05-16 15:33:45 +0530

    Add files via upload

---
+++ govar.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main()  {
+  fmt.Println("GO"+"language")
+  fmt.Println("7+6 =", 7+6)
+  fmt.Println("13.0/4.0 = ", 13.0/4.0)
+  fmt.Println(false && true)
+  fmt.Println(true || false)
+  fmt.Println(!false)
+}
